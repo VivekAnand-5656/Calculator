280683bb6038e8e1c927443571418024606f4497
Vivek Anand <va691187@gmail.com>
2025-07-01 00:53:57 +0530

    Update Responsive

--- calculator.js
+++ calculator.js
@@ -1,10 +1,33 @@
 let display = document.getElementById("result");
-function valueAdd(value){
-    display.value += value;
+
+function valueAdd(value) {
+  display.value += value;
 }
-function clearDisplay(){
-    display.value = "";
+
+function clearDisplay() {
+  display.value = "";
 }
-function calculate(){
-    display.value=eval(display.value);
+
+function calculate() {
+  display.value = eval(display.value);
 }
+
+// Responsive code
+function makeResponsive() {
+  const calc = document.querySelector(".calculator");
+  const width = window.innerWidth;
+
+  if (width <= 768) {
+    calc.style.width = "90%";
+    calc.style.height = "70%";
+  } else if (width <= 1024) {
+    calc.style.width = "60%";
+    calc.style.height = "75%";
+  } else {
+    calc.style.width = "30%";
+    calc.style.height = "80%";
+  }
+}
+
+window.addEventListener("load", makeResponsive);
+window.addEventListener("resize", makeResponsive);
